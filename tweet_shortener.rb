a4e65322a257314c77fba8665e9df063314222b5
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-27 13:55:10 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+def dictionary
+  swaptionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweetarray = tweet.split(" ")
+  tweetarray.each_with_index do |word, index|
+    check = word.downcase
+    if dictionary.include?(check)
+      tweetarray[index] = dictionary[check]
+    end
+  end
+  tweetarray.join(" ")
+end
+
+def bulk_tweet_shortener(arrayoftweets)
+  arrayoftweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(arrayoftweets)
+  bulk_tweet_shortener(arrayoftweets.collect {|tweet| tweet.length > 140})
+end
